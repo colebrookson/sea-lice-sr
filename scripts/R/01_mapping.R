d6dcf2cbe1ef70db6cbc00a3b0345ad836cc9480
Cole Brookson <cole.brookson@gmail.com>
2022-03-09 18:20:40 -0500

    add a bunch of data, plus R stuff

---
+++ scripts/R/01_mapping.R
@@ -0,0 +1,14 @@
+########## 
+##########
+# Look at locations of all data present to compare farm sampling and wild 
+# sampling 
+##########
+##########
+# AUTHOR: Cole B. Brookson
+# DATE OF CREATION: 2022-02-02
+##########
+##########
+
+# set up =======================================================================
+
+install.packages("tidyverse")
