a72ef6ea863fc2c21eb90217eea3713426979651
Cole Brookson <cole.brookson@gmail.com>
2022-03-17 16:49:45 -0400

    git set up models

--- scripts/03_stan_model_fits.R
+++ scripts/03_stan_model_fits.R
@@ -18,14 +18,15 @@ library(rstanarm)
 library(glmmTMB)
 
 farm_regress = read_csv(here("./data/regression-data/farm-regression-data.csv"))
-scfs_regress = read_csv(here("./data/regression-data/scfs-regression-data.csv"))
+scfs_regress = read_csv(
+    here("./data/regression-data/scfs-regression-leps-include-chals-data.csv"))
 
 # ensure all counts are integer valued
 scfs_regress$all_lep = as.integer(scfs_regress$all_lep)
 scfs_regress$all_cal = as.integer(scfs_regress$all_cal)
 scfs_regress$all_lice = as.integer(scfs_regress$all_lice)
 scfs_regress$year = as.factor(as.character(scfs_regress$year))
-scfs_regress$week = as.factor(as.character(scfs_regress$week))
+scfs_regress$farm_year = as.factor(as.character(scfs_regress$farm_year))
 scfs_regress$farm = as.factor(as.character(scfs_regress$farm))
 
 # regresion ====================================================================
@@ -53,22 +54,22 @@ plot(stan_glm_poi, plotfun = "trace")
 # use glmmTMB to do this as well ===============================================
 
 # fit models using the different approaches 
-tmb_glmm_poi = glmmTMB(all_lep ~ year + (1 | week) + (1 | farm), 
+tmb_glmm_poi = glmmTMB(all_lep ~ year + (1 | week) + (1 | farm_year), 
                     data = scfs_regress, 
                     family = poisson,
                     ziformula =  ~0
 )
-tmb_glmm_zip = glmmTMB(all_lep ~ year + (1 | week) + (1 | farm), 
+tmb_glmm_zip = glmmTMB(all_lep ~ year + (1 | week) + (1 | farm_year), 
                     data = scfs_regress, 
                     family = poisson,
                     ziformula =  ~1
 )
-tmb_glmm_nb = glmmTMB(all_lep ~ year + (1 | week) + (1 | farm), 
+tmb_glmm_nb = glmmTMB(all_lep ~ year + (1 | week) + (1 | farm_year), 
                     data = scfs_regress, 
                     family = nbinom2,
                     ziformula =  ~0
 )
-tmb_glmm_zinb = glmmTMB(all_lep ~ year + (1 | week) + (1 | farm), 
+tmb_glmm_zinb = glmmTMB(all_lep ~ year + (1 | week) + (1 | farm_year), 
                     data = scfs_regress, 
                     family = nbinom2,
                     ziformula =  ~1
