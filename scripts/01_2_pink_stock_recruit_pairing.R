5223984857e711d2eed78b32e222750af243607e
colebrookson <cole.brookson@gmail.com>
2022-06-13 18:47:21 -0400

    small changes

---
+++ scripts/01_2_pink_stock_recruit_pairing.R
@@ -0,0 +1,27 @@
+##########
+# AUTHOR: Cole B. Brookson
+# DATE OF CREATION: 2022-02-02
+##########
+##########
+
+# set up =======================================================================
+
+library(tidyverse)
+library(here)
+
+# pull in file with all functions to clean data 
+source(here::here("./src/02_data_cleaning_funs.R"))
+source(here::here("./src/01_plot_themes.R"))
+
+nuseds = read_csv(here::here("./data/NuSEDS/NuSEDS_20220309.csv"))
+
+# trim nuseds data to useful size ==============================================
+names(nuseds)
+
+names_keep = c("AREA", "WATERBODY", "POPULATION", "RUN_TYPE", "WATERSHED_CDE",
+               "SPECIES", "ANALYSIS_YR", "MAX_ESTIMATE", "ADULT_PRESENCE")
+nuseds = nuseds %>% 
+  select(all_of(names_keep)) %>% 
+  filter(SPECIES == "Pink") %>% 
+  filter(AREA %in% c("7", "8", "9", "10"))
+
