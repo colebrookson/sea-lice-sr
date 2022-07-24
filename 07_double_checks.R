16d5ef1edd27d61f9e0aac78409a2b6abfd5ecfe
Cole Brookson <cole.brookson@gmail.com>
2022-07-24 17:14:05 -0400

    new script for making sure things look a particular way

---
+++ 07_double_checks.R
@@ -0,0 +1,10 @@
+##########
+# AUTHOR: Cole B. Brookson
+# DATE OF CREATION: 2022-07-21
+##########
+##########
+
+# set up =======================================================================
+
+library(tidyverse)
+library(here)
