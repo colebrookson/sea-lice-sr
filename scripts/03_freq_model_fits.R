e4765898d43ff0773ad2ef03c56fb4c6b856461f
colebrookson <cole.brookson@gmail.com>
2026-03-17 15:44:01 -0400

    file deleted here

--- scripts/03_freq_model_fits.R
+++ scripts/03_freq_model_fits.R
@@ -1,6 +1,6 @@
-########## 
 ##########
-# Pull in all data for cleaning 
+##########
+# Pull in all data for cleaning
 ##########
 ##########
 # AUTHOR: Cole B. Brookson
@@ -15,65 +15,82 @@ library(here)
 library(glmmTMB)
 library(DHARMa)
 
-farm_regress = read_csv(
-    here::here("./data/farm-data/clean/marty-bati-data-joined-stocked-only.csv"))
+farm_regress <- read_csv(
+    here::here("./data/clean-farm/marty-bati-data-joined-stocked-only.csv")
+)
 
-scfs_regress = read_csv(
+scfs_regress <- read_csv(
     here::here(
-        "./data/prepped-data/scfs-regression-leps-include-chals-data.csv"))
+        "./data/regression-data/scfs-regression-leps-include-chals-data.csv"
+    )
+)
 
 # ensure all counts are integer valued
-scfs_regress$all_lep = as.integer(scfs_regress$all_lep)
-scfs_regress$all_cal = as.integer(scfs_regress$all_cal)
-scfs_regress$all_lice = as.integer(scfs_regress$all_lice)
-scfs_regress$year = as.factor(as.character(scfs_regress$year))
-scfs_regress$farm_name = as.factor(as.character(scfs_regress$farm_name))
-scfs_regress$week = as.factor(as.character(scfs_regress$week))
+scfs_regress$all_lep <- as.integer(scfs_regress$all_lep)
+scfs_regress$all_cal <- as.integer(scfs_regress$all_cal)
+scfs_regress$all_lice <- as.integer(scfs_regress$all_lice)
+scfs_regress$year <- as.factor(as.character(scfs_regress$year))
+scfs_regress$farm_name <- as.factor(as.character(scfs_regress$farm_name))
+scfs_regress$week <- as.factor(as.character(scfs_regress$week))
+
+scfs_regress_2001 <- scfs_regress %>% dplyr::filter(year == 2001)
+nrow(scfs_regress_2001)
+summary(scfs_regress_2001$all_lep)
+
+
 
 # use glmmTMB ==================================================================
 
 # set up parallel fitting option
-n_cores = min(parallel::detectCores(), 8)
+n_cores <- min(parallel::detectCores(), 8)
 
-# fit models using the different approaches 
+# fit models using the different approaches
 
 # approach 1 - location as random effect
-tmb_glmm_1_nb = glmmTMB(all_lep ~ year + (1 | week) + (1 | farm_name),
-                    data = scfs_regress,
-                    family = nbinom2,
-                    ziformula =  ~0,
-                    control = glmmTMBControl(optimizer = optim,
-                                             optArgs = list(method = "BFGS"),
-                                             parallel = n_cores)
+tmb_glmm_1_nb <- glmmTMB(all_lep ~ year + (1 | week) + (1 | farm_name),
+    data = scfs_regress,
+    family = nbinom2,
+    ziformula = ~0,
+    control = glmmTMBControl(
+        optimizer = optim,
+        optArgs = list(method = "BFGS"),
+        parallel = n_cores
+    )
 )
 saveRDS(tmb_glmm_1_nb, here::here("./outputs/model-outputs/tmb-glmm-ap1-nb.RDS"))
-tmb_glmm_1_zinb = glmmTMB(all_lep ~ year + (1 | week) + (1 | farm_name),
-                    data = scfs_regress,
-                    family = nbinom2,
-                    ziformula =  ~1,
-                    control = glmmTMBControl(optimizer = optim,
-                                             optArgs = list(method = "BFGS"),
-                                             parallel = n_cores)
+tmb_glmm_1_zinb <- glmmTMB(all_lep ~ year + (1 | week) + (1 | farm_name),
+    data = scfs_regress,
+    family = nbinom2,
+    ziformula = ~1,
+    control = glmmTMBControl(
+        optimizer = optim,
+        optArgs = list(method = "BFGS"),
+        parallel = n_cores
+    )
 )
 saveRDS(tmb_glmm_1_zinb, here::here("./data/model-outputs/tmb-glmm-ap1-zinb.RDS"))
 
 # aproach 2 - location as fixed effect
-tmb_glmm_2_nb = glmmTMB(all_lep ~ year + farm_name + (1 | week),
-                    data = scfs_regress,
-                    family = nbinom2,
-                    ziformula =  ~0,
-                    control = glmmTMBControl(optimizer = optim,
-                                             optArgs = list(method = "BFGS"),
-                                             parallel = n_cores)
+tmb_glmm_2_nb <- glmmTMB(all_lep ~ year + farm_name + (1 | week),
+    data = scfs_regress,
+    family = nbinom2,
+    ziformula = ~0,
+    control = glmmTMBControl(
+        optimizer = optim,
+        optArgs = list(method = "BFGS"),
+        parallel = n_cores
+    )
 )
 saveRDS(tmb_glmm_2_nb, here::here("./data/model-outputs/tmb-glmm-ap2-nb.RDS"))
-tmb_glmm_2_zinb = glmmTMB(all_lep ~ year + farm_name + (1 | week),
-                    data = scfs_regress,
-                    family = nbinom2,
-                    ziformula =  ~1,
-                    control = glmmTMBControl(optimizer = optim,
-                                             optArgs = list(method = "BFGS"),
-                                             parallel = n_cores)
+tmb_glmm_2_zinb <- glmmTMB(all_lep ~ year + farm_name + (1 | week),
+    data = scfs_regress,
+    family = nbinom2,
+    ziformula = ~1,
+    control = glmmTMBControl(
+        optimizer = optim,
+        optArgs = list(method = "BFGS"),
+        parallel = n_cores
+    )
 )
 saveRDS(tmb_glmm_2_zinb, here::here("./data/model-outputs/tmb-glmm-ap2-zinb.RDS"))
 
@@ -81,70 +98,78 @@ saveRDS(tmb_glmm_2_zinb, here::here("./data/model-outputs/tmb-glmm-ap2-zinb.RDS"
 AIC(tmb_glmm_1_nb, tmb_glmm_1_zinb, tmb_glmm_2_nb, tmb_glmm_2_zinb)
 
 ## BEGIN NOTE ###############################
-# So we see that the best models for both the approach 1 and approach 2 options 
-# are the non-zero inflated options (probably since it's a type II negative 
-# binomial), so we'll stick with those for now. IMPORTANTLY, the difference in 
-# AIC between approach 1) and 2) isn't that large (~10 AIC points) so the 
+# So we see that the best models for both the approach 1 and approach 2 options
+# are the non-zero inflated options (probably since it's a type II negative
+# binomial), so we'll stick with those for now. IMPORTANTLY, the difference in
+# AIC between approach 1) and 2) isn't that large (~10 AIC points) so the
 # previously-used method will be retained.
 ## END NOTE ###############################
 
 # compare methods for optimization
 
 # default method is the one from above - quasi-Newtonian
-tmb_bfgs = tmb_glmm_1_nb
+tmb_bfgs <- tmb_glmm_1_nb
 
-# CG method is conjugate gradient method 
-tmb_cg = glmmTMB(all_lep ~ year + (1 | week) + (1 | farm),
-                    data = scfs_regress,
-                    family = nbinom2,
-                    ziformula =  ~0,
-                    control = glmmTMBControl(optimizer = optim,
-                                             optArgs = list(method = "CG"),
-                                             parallel = n_cores)
+# CG method is conjugate gradient method
+tmb_cg <- glmmTMB(all_lep ~ year + (1 | week) + (1 | farm),
+    data = scfs_regress,
+    family = nbinom2,
+    ziformula = ~0,
+    control = glmmTMBControl(
+        optimizer = optim,
+        optArgs = list(method = "CG"),
+        parallel = n_cores
+    )
 )
 saveRDS(tmb_cg, here::here("./data/model-outputs/tmb-cg.RDS"))
 
 # L-BFGS-B is a limited-memory modification of the BFGS quasi-Newtonian method
-tmb_lbfgsb = glmmTMB(all_lep ~ year + (1 | week) + (1 | farm),
-                    data = scfs_regress,
-                    family = nbinom2,
-                    ziformula =  ~0,
-                    control = glmmTMBControl(optimizer = optim,
-                                             optArgs = list(
-                                                 method = "L-BFGS-B"),
-                                             parallel = n_cores)
+tmb_lbfgsb <- glmmTMB(all_lep ~ year + (1 | week) + (1 | farm),
+    data = scfs_regress,
+    family = nbinom2,
+    ziformula = ~0,
+    control = glmmTMBControl(
+        optimizer = optim,
+        optArgs = list(
+            method = "L-BFGS-B"
+        ),
+        parallel = n_cores
+    )
 )
 saveRDS(tmb_lbfgsb, here::here("./data/model-outputs/tmb-lbfgsb.RDS"))
 
-# SANN is a version of simulated annealing 
-tmb_sann = glmmTMB(all_lep ~ year + (1 | week) + (1 | farm),
-                    data = scfs_regress,
-                    family = nbinom2,
-                    ziformula =  ~0,
-                    control = glmmTMBControl(optimizer = optim,
-                                             optArgs = list(
-                                                 method = "SANN"),
-                                             parallel = 10)
+# SANN is a version of simulated annealing
+tmb_sann <- glmmTMB(all_lep ~ year + (1 | week) + (1 | farm),
+    data = scfs_regress,
+    family = nbinom2,
+    ziformula = ~0,
+    control = glmmTMBControl(
+        optimizer = optim,
+        optArgs = list(
+            method = "SANN"
+        ),
+        parallel = 10
+    )
 )
 saveRDS(tmb_sann, here::here("./data/model-outputs/tmb-sann.RDS"))
 
-tmb_bfgs = readRDS(here::here("./data/model-outputs/tmb-glmm-ap1-nb.RDS"))
-tmb_cg = readRDS(here::here("./data/model-outputs/tmb-cg.RDS"))
-tmb_lbfgsb = readRDS(here::here("./data/model-outputs/tmb-lbfgsb.RDS"))
-tmb_sann = readRDS(here::here("./data/model-outputs/tmb-sann.RDS"))
+tmb_bfgs <- readRDS(here::here("./data/model-outputs/tmb-glmm-ap1-nb.RDS"))
+tmb_cg <- readRDS(here::here("./data/model-outputs/tmb-cg.RDS"))
+tmb_lbfgsb <- readRDS(here::here("./data/model-outputs/tmb-lbfgsb.RDS"))
+tmb_sann <- readRDS(here::here("./data/model-outputs/tmb-sann.RDS"))
 
 AIC(tmb_bfgs, tmb_cg, tmb_lbfgsb, tmb_sann)
 
 # read in and inspect model objects ============================================
 
 # read in model objects
-tmb_1 = readRDS(here::here("./data/model-outputs/tmb-glmm-ap1-nb.RDS"))
-tmb_2 = readRDS(here::here("./data/model-outputs/tmb-glmm-ap2-nb.RDS"))
+tmb_1 <- readRDS(here::here("./data/model-outputs/tmb-glmm-ap1-nb.RDS"))
+tmb_2 <- readRDS(here::here("./data/model-outputs/tmb-glmm-ap2-nb.RDS"))
 
 # simulate residuals
-tmb_1_simres = simulateResiduals(tmb_1)
-tmb_2_simres = simulateResiduals(tmb_2)
+tmb_1_simres <- simulateResiduals(tmb_1)
+tmb_2_simres <- simulateResiduals(tmb_2)
 
-# plot residuals 
+# plot residuals
 plot(tmb_1_simres)
 plot(tmb_2_simres)
