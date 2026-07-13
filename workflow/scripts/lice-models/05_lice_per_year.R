932ed5235871adca7b9d96b106abdbbeea9cf6a0
colebrookson <cole.brookson@gmail.com>
2026-07-13 20:46:02 +0000

    ok i think the model is set up correctly now?

--- workflow/scripts/lice-models/05_lice_per_year.R
+++ workflow/scripts/lice-models/05_lice_per_year.R
@@ -5,6 +5,7 @@ source(here::here("./workflow/scripts/functions/theme_better.R"))
 source(here::here("./workflow/scripts/functions/global.R"))
 library(magrittr)
 library(ggplot2)
+library(nimble)
 
 collated_df <- readr::read_csv(
     here::here("./data/scfs-data/clean/lice-counts-for-regression.csv")
@@ -14,38 +15,61 @@ collated_df <- readr::read_csv(
 table(collated_df$week)
 table(collated_df$location)
 
+# super quick PPC on the gamma
+# r_draws <- rgamma(n = 2000, shape = 1, rate = 0.5)
+# sim <- rnbinom(n = 2000, size = r_draws, mu = mean(collated_df$all_leps))
+
+# quantile(sim, c(0.5, 0.9, 0.99, 1))
+# quantile(collated_df$all_leps, c(0.5, 0.9, 0.99, 1))
+
 # make the model itself
 glmm_mod <- nimble::nimbleCode({
-    # priors for the fixed effects (just year) ---------------------------------
-    beta_0 ~ dnorm(0, sd = 10) # the intercept 
-    beta_1 ~ dnorm(0, sd = 10) # this is the year effect 
+    # year as cell means, no intercept ----------------------------------------
+    for (k in 1:Yr) {
+        beta_year[k] ~ dnorm(0, sd = 10)
+    }
 
     # prior for the overdispersion ---------------------------------------------
-    r ~ dgamma(shape = 0.01, rate = 0.01) # must be > 0
+    #' I picked this by looking at mean(collated_df$all_leps) = ~0.83 
+    #' because here, the shape and rate a and b have mean a/b and variance 
+    #' a/b^2. So then var(collated_df$all_leps) = ~4.79. So given that, 
+    #' Gamma with mean 2, sd 2 gives shape = 1, rate = 0.5.
+    r ~ dgamma(shape = 1, rate = 0.5) # must be > 0
 
     # prior for random effects -------------------------------------------------
     sigma_week ~ dunif(0, 5) # trying to shrink week 9 (3 obs)
     sigma_location ~ dunif(0, 2) # regularizing because only 3 groups
 
     # loop for the random effects
-    for(i in 1:W) {
-        b_week[i] ~ dnorm(0, sd = sigma_week)
+    for (i in 1:W) { 
+        # this is the non-centered parameterization which we need because
+        # some groups are really thinly observed
+        z_week[i] ~ dnorm(0, sd = 1)
+        b_week[i] <- sigma_week * z_week[i]
     }
     for(j in 1:L) {
-        b_location[i] ~ dnorm(0, sd = sigma_location)
+        b_location[j] ~ dnorm(0, sd = sigma_location)
     }
 
     # the likelihood -----------------------------------------------------------
     for (i in 1:N) {
         # get the linear predictor on the log scale 
-        log(mu[i]) <- beta_0 + inprod(beta_1, X[i, P]) +
+        #' NOTE: we don't want a beta_0 here because that gives each year it's 
+        #' own free coefficient. So, it's just indexed directly like this: 
+        #'              beta_1[year_idx[i]]
+        #' The other option is year as a random effect, where 
+        #' beta_year[k] ~ dnorm(mu_year, sd = sigma_year), which partially pools
+        #' each year toward each other, which gives a variance component for 
+        #' interannual variability. 
+
+        log(mu[i]) <- beta_year[year_idx[i]] + 
                           b_week[week_idx[i]] + b_location[location_idx[i]]
 
         # convert expected count (mu) to probability parameter
         p[i] <- r / (r + mu[i])
         
         # now feed this into the actually poisson bit
-        y[k] ~ dnegbin(p[i], r)
+        Y[i] ~ dnegbin(p[i], r)
     }
 })
 
@@ -57,11 +81,12 @@ glmm_mod <- nimble::nimbleCode({
 # define the constants 
 constants <- list(
     N = nrow(collated_df), 
-    P = ncol(collated_df[, "year"]), # just one here
     W = length(unique(collated_df$week)),
     L = length(unique(collated_df$location)),
+    Yr = length(unique(collated_df$year)),
     week_idx = as.numeric(as.factor(collated_df$week)),
-    location_idx = as.numeric(as.factor(collated_df$location))
+    location_idx = as.numeric(as.factor(collated_df$location)), 
+    year_idx = as.numeric(as.factor(collated_df$year))
 )
 
 # define the data 
@@ -71,13 +96,12 @@ data_list <- list(
 
 # define initial values 
 inits <- list(
-    beta_0 = 0, 
-    beta_1 = 0, 
-    r = 10, 
+    beta_year = rep(0, constants$Yr),
+    r = 1,
     sigma_location = 1, 
     sigma_week = 1, 
-    b_week = rnorm(0, 0.1), 
-    b_location = rnorm(0, 0.1)
+    z_week = rnorm(n = constants$W, mean = 0, sd = 0.1), # instead of b_week
+    b_location = rnorm(n = constants$L, mean = 0, sd = 0.1)
 )
 
 nb_model <- nimble::nimbleModel(
@@ -86,19 +110,19 @@ nb_model <- nimble::nimbleModel(
     inits = inits, 
     data = data_list
 )
+C_nb_model <- nimble::compileNimble(nb_model)
 
 # the MCMC gets configured here
 nb_mcmc_config <- nimble::configureMCMC(nb_model, 
     monitors = c(
-        "beta_0", "beta_1", "b_week", "b_location", 
-        "sigma_week", "sigma_location"
+        "beta_year", "b_week", "b_location",
+        "sigma_week", "sigma_location", "r"
     ) 
 )
 
 # time to compile!
 nb_mcmc <- nimble::buildMCMC(nb_mcmc_config)
-C_nb_model <- nimble::compileNimble(nb_model)
-C_nm_mcmc <- nimble::compileNimble(nb_mcmc)
+C_nb_mcmc <- nimble::compileNimble(nb_mcmc, project = nb_model)
 
 # run and sample this thang
 samples <- nimble::runMCMC(C_nb_mcmc, niter = 10000, nburnin = 2000, thin = 5)
