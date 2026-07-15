94d11cde9fcec5c9f93cc2cd0af1cfb18e3ca6f6
colebrookson <cole.brookson@gmail.com>
2026-07-15 03:31:33 +0000

    a bug here

--- workflow/scripts/lice-models/03_impute_lice.R
+++ workflow/scripts/lice-models/03_impute_lice.R
@@ -63,3 +63,68 @@ readr::write_csv(
 
 # get a single point-estimate value --------------------------------------------
 #' I think the best way to do this is to fit to all M and pool the answer... 
+#' but i'm not gonna do that atm its too much work so for now: 
+readr::write_csv(
+    replicates %>% dplyr::filter(rep == 1) %>% dplyr::select(-rep),
+    here::here("./data/scfs-data/clean/lice-counts-for-regression.csv")
+)
+
+# so now, just a quick check ---------------------------------------------------
+# need to make sure all the numbers add up !!
+
+
+
+
+#' FIX THIS!!!!!!!!
+
+
+
+chk <- replicates %>%
+    dplyr::filter(rep == 1) %>%
+    dplyr::mutate(
+        mot_ok = dplyr::near(lep_mot + cal_mot_tot, all_mot),
+        cope_ok = dplyr::near(lep_cope + cal_cope_tot, all_cope),
+        chal_ok = dplyr::near(lep_chal + cal_chal, all_chal)
+    ) %>%
+    # a stage passes if it balances OR both sides are NA (stage not counted)
+    dplyr::mutate(
+        mot_ok = mot_ok  | (is.na(lep_mot + cal_mot_tot) & is.na(all_mot)),
+        cope_ok = cope_ok | (is.na(lep_cope + cal_cope_tot) & is.na(all_cope)),
+        chal_ok = chal_ok | (is.na(lep_chal + cal_chal) & is.na(all_chal))
+    ) %>%
+    dplyr::filter(!(mot_ok & cope_ok & chal_ok))
+
+if (nrow(chk) > 0) {
+    stop(nrow(chk), " fish fail per-stage conservation.")
+}
+
+
+
+
+
+r1 <- replicates %>% dplyr::filter(rep == 1) %>%
+    dplyr::mutate(
+        recovered = rowSums(cbind(
+            lep_mot, cal_mot_tot, lep_cope, cal_cope_tot, lep_chal, cal_chal
+        ), na.rm = TRUE),
+        diff = recovered - all_lice
+    )
+
+# where do the mismatches live?
+r1 %>% dplyr::filter(abs(diff) > 1e-8) %>% dplyr::count(year)
+
+# and pull one to inspect the components
+r1 %>% dplyr::filter(abs(diff) > 1e-8) %>%
+    dplyr::select(year, all_lice, recovered, diff,
+                  lep_mot, cal_mot_tot, all_mot,
+                  lep_cope, cal_cope_tot, all_cope,
+                  lep_chal, cal_chal, all_chal) %>%
+    dplyr::slice_head(n = 5) %>%
+    as.data.frame()
+
+r1 %>%
+    dplyr::filter(year == 2001, all_mot > 0, is.na(lep_mot)) %>%
+    dplyr::select(year, all_mot, unid_mot_n, lep_mot_obs, cal_mot_obs,
+                  p_mot, lep_mot_imp) %>%
+    dplyr::slice_head(n = 5) %>%
+    as.data.frame()
